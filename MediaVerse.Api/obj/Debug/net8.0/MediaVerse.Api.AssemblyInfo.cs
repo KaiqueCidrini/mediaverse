6cd3136e693b573caebb6bbedfa98eb30e28af93
Kaique Cidrini <contatokaiquecidrini@gmail.com>
2025-10-02 19:39:53 -0300

    Some stuff

--- MediaVerse.Api/obj/Debug/net8.0/MediaVerse.Api.AssemblyInfo.cs
+++ MediaVerse.Api/obj/Debug/net8.0/MediaVerse.Api.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MediaVerse.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0bac661b4e833f8d9ad5f681b25b0ddf3758388b")]
 [assembly: System.Reflection.AssemblyProductAttribute("MediaVerse.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MediaVerse.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- MediaVerse.Application/obj/Debug/net8.0/MediaVerse.Application.AssemblyInfo.cs
+++ MediaVerse.Application/obj/Debug/net8.0/MediaVerse.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MediaVerse.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0bac661b4e833f8d9ad5f681b25b0ddf3758388b")]
 [assembly: System.Reflection.AssemblyProductAttribute("MediaVerse.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MediaVerse.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
